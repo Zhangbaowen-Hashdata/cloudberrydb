7f6c0705e4add44b0680c938676d23747da14cb9
zhangbaowen <zhangbaowen@hashdata.cn>
2024-05-16 14:53:48 +0800

    测试新代码

---
+++ concourse/scripts/update_tag_number.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# 获取最新的 tag
+echo "工作空间是 $GITHUB_WORKSPACE"
+
+
+#rm -rf /tmp/release/*
+#target_dir="/tmp/zbw-test-actions"
+#timestamp=$(date +"%Y%m%d")
+#
+## 检查目标目录是否存在
+#if [ -d "$target_dir" ]; then
+#    # 如果目录存在，则删除目录
+#    rm -rf "$target_dir"
+#    echo "已删除目录 $target_dir"
+#else
+#    echo "目录 $target_dir 不存在"
+#fi
+
+# 克隆仓库到目标目录
+#cd /tmp/
+
+#git clone git@github.com:Zhangbaowen-Hashdata/zbw-test-actions.git
+#echo "克隆成功"
+#cd /tmp/zbw-test-actions
+pwd
+ls -al
+
+latest_tag=$(git tag | sort -V -r | head -n 1)
+
+echo "已存在tag最新一条为 $latest_tag"
+# 从最新的 tag 中提取版本号部分作为变量 version
+
+main_version=$(echo $latest_tag | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+')
+echo "最新的cbdb主版本是$main_version"
+
+new_tag="$main_version.$timestamp-nightly"
+
+echo "新的 nightly 版本号为: $new_tag"
+
+cd $GITHUB_WORKSPACE
